e2299b7d6e4a2f1be47a6f7d47b464395404f5ca
soumyaranjanpuhan <soumyaranjan.puhan@33crores.com>
2025-04-24 17:57:40 +0530

    refactor: Simplify status update logic for clarity

--- resources/views/website/view-all-niti.blade.php
+++ resources/views/website/view-all-niti.blade.php
@@ -218,10 +218,9 @@
             $status = $niti['niti_status'];
             $side = $index % 2 === 0 ? 'left' : 'right';
 
-            if ($status === 'Started') {
-                $status = "On Going";
-            }
-
+        if ($status === 'Started') {
+            $status = 'On Going';
+        }
             $icon = match ($status) {
                 'Completed' => 'fa-check-circle',
                 'Started' => 'fa-sun',
