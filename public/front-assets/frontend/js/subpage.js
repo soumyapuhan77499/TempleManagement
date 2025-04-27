a931b077335c8150bf6db3dbe0853c4f2d352bf2
soumyaranjanpuhan <soumyaranjan.puhan@33crores.com>
2025-04-27 13:06:51 +0530

    style: Implement responsive mobile navigation with hamburger icon and improve header layout

---
+++ public/front-assets/frontend/js/subpage.js
@@ -0,0 +1,32 @@
+   
+
+// convience modal
+    
+        function openModal(id) {
+            document.getElementById('modal-' + id).classList.remove('hidden');
+        }
+
+        function closeModal(id) {
+            document.getElementById('modal-' + id).classList.add('hidden');
+        }
+
+        function openDescModal(id) {
+            document.getElementById('desc-modal-' + id).classList.remove('hidden');
+        }
+
+        function closeDescModal(id) {
+            document.getElementById('desc-modal-' + id).classList.add('hidden');
+        }
+    
+        function openPhotoModal(id) {
+            document.getElementById('photo-modal-' + id).classList.remove('hidden');
+        }
+
+        function closePhotoModal(id) {
+            document.getElementById('photo-modal-' + id).classList.add('hidden');
+        }
+
+    
+
+
+
--- resources/views/partials/header-puri-dham.blade.php
+++ resources/views/partials/header-puri-dham.blade.php
@@ -3,14 +3,46 @@
         <div class="logo">
             <img src="{{ asset('website/logo.png') }}" alt="logo">
         </div>
+
+        <!-- Desktop Menu -->
         <nav class="nav-menu">
             <a href="#">Nitis</a>
-            <span class="separator">SM <a href="#" class="live-badges"><i class="fa fa-bolt"></i>
-                    Live</a></span>
+            <span class="separator">SM <a href="#" class="live-badges"><i class="fa fa-bolt"></i> Live</a></span>
             <a href="#">Services</a>
             <a href="#">Nearby Temples</a>
             <a href="#">Conveniences</a>
             <a href="#">Temple Information</a>
         </nav>
+
+        <!-- Hamburger Icon (Mobile) -->
+        <div class="hamburger-icon" onclick="openMobileMenu()">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
     </div>
 </header>
+
+<!-- Mobile Nav -->
+<nav id="mobileNav" class="mobile-nav">
+    <div class="nav-close" onclick="closeMobileMenu()"><i class="fa fa-times"></i></div>
+    <ul>
+        <li><a href="#">Nitis</a></li>
+        <li><a href="#">SM <span class="live-badges"><i class="fa fa-bolt"></i> Live</span></a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Nearby Temples</a></li>
+        <li><a href="#">Conveniences</a></li>
+        <li><a href="#">Temple Information</a></li>
+    </ul>
+</nav>
+
+<script>
+    
+     function openMobileMenu() {
+        document.getElementById('mobileNav').classList.add('active');
+    }
+
+    function closeMobileMenu() {
+        document.getElementById('mobileNav').classList.remove('active');
+    }
+</script>
