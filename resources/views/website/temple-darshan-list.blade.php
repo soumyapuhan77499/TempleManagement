60c603c83ab0f37ee2b507e26edb12f43d1ab706
soumyaranjanpuhan <soumyaranjan.puhan@33crores.com>
2025-04-27 15:00:46 +0530

    Implement code changes to enhance functionality and improve performance

--- resources/views/website/temple-darshan-list.blade.php
+++ resources/views/website/temple-darshan-list.blade.php
@@ -226,7 +226,7 @@
 
     <!-- Hero Section -->
     <div class="hero">
-        <img class="hero-bg" src="{{ asset('website/parking.jpeg') }}" alt="Mandir Background" />
+        <img class="hero-bg" src="{{ asset('website/darshans.jpg') }}" alt="Mandir Background" />
         <div class="hero-overlay"></div>
         <div class="hero-content">
             <h1>Temple Darshan Timeline</h1>
