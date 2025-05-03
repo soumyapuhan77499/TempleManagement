1029103da1578870ea2dd357c6abf1fbf9e163b0
soumyaranjanpuhan <soumyaranjan.puhan@33crores.com>
2025-05-03 16:15:53 +0530

    ddd

--- routes/api.php
+++ routes/api.php
@@ -273,6 +273,9 @@ Route::controller(TempleNitiController::class)->group(function () {
   Route::post('/update-upcoming', 'updateActiveNitiToUpcoming');
   Route::get('/sub-niti/running','getRunningSubNitis');
   Route::post('/store-other-niti', 'storeTextOtherNiti');
+  Route::post('/niti/delete-other/{id}', 'deleteOtherNiti');
+  Route::get('/latest-apk', 'latestApk');
+
 
   // Protected routes (niti_admin must be authenticated)
   Route::middleware('auth:niti_admin')->group(function () {
@@ -284,8 +287,7 @@ Route::controller(TempleNitiController::class)->group(function () {
       Route::post('/sub-niti/add', 'addAndStartSubNiti');
       Route::post('/update-sub-niti-name/{id}', 'updateSubNitiName');
       Route::delete('/delete-sub-niti/{id}','softDeleteSubNiti');
-      Route::post('/start-other-niti', 'startOtherNiti');
-      Route::post('/stop-other-niti', 'stopOtherNiti');
+      Route::post('/niti/delete-other/{id}', 'deleteOtherNiti');
   });
 
   Route::post('/save-temple-news', 'storeByNoticeName');
