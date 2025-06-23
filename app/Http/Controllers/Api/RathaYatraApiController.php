0e956eb32485e0bdc7c843eda4c491a0a77d7945
soumyaranjanpuhan <soumyaranjan.puhan@33crores.com>
2025-06-23 18:47:25 +0530

    f

--- app/Http/Controllers/Api/RathaYatraApiController.php
+++ app/Http/Controllers/Api/RathaYatraApiController.php
@@ -534,7 +534,6 @@ public function getOtherNiti()
     }
 }
 
-
 public function resetNiti(Request $request)
 {
     try {
--- app/Models/RathaYatraNiti.php
+++ app/Models/RathaYatraNiti.php
@@ -21,12 +21,12 @@ class RathaYatraNiti extends Model
         'niti_type',
         'start_time',
         'end_time',
-        'start_user_id',           
-        'end_user_id',           
+        'start_user_id',
+        'end_user_id',
         'start_time_edit_user_id',           
         'end_time_edit_user_id',           
         'niti_not_done_reason',
         'not_done_user_id',           
-        'niti_status',           
+        'niti_status',
     ];
 }
